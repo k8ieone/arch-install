a2919e9534a63ff70e44e345afbdb22004b1b387
k8ie <k8ie@mcld.eu>
2020-10-15 16:15:02 +0200

    Fixed wrong filename

--- x86_64/07_grub.sh
+++ x86_64/07_grub.sh
@@ -13,11 +13,11 @@ then
     pacman -S --noconfirm grub efibootmgr which
     cd /
     grub-install --target=x86_64-efi --efi-directory=boot --bootloader-id=Arch
-    cp ~/rugl/bash/arch-setuptools/configs/system-wide/grub /etc/default/grub
+    cp ~/arch-install/configs/system-wide/grub /etc/default/grub
     grub-mkconfig -o /boot/grub/grub.cfg
 else
     pacman -S --noconfirm grub which
     grub-install --target=i386-pc /dev/$1
-    cp ~/rugl/bash/arch-setuptools/configs/system-wide/grub /etc/default/grub
+    cp ~/arch-install/configs/system-wide/grub /etc/default/grub
     grub-mkconfig -o /boot/grub/grub.cfg
 fi
