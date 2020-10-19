3ef53f50774c3282921856754d7f036105c2bb7b
satcom886 <rostik.medved@gmail.com>
2020-10-19 18:08:11 +0200

    Improved readability

--- install_x86_64.sh
+++ install_x86_64.sh
@@ -115,6 +115,7 @@ echo "What setup type do you want?"
 echo "Available options are:"
 echo "1 - satcom886's headless setup"
 echo "2 - satcom886's Plasma setup"
+echo -n "You choice: "
 read -r SETUPTYPE
 
 # Below, we actually start doing stuff.
