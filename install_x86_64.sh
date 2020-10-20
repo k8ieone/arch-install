f1afb1b61dd21d0bbc49463c5ec9be204710594c
k8ie <k8ie@mcld.eu>
2020-10-20 19:24:52 +0200

    Don't run makepkg as root

--- install_x86_64.sh
+++ install_x86_64.sh
@@ -172,7 +172,7 @@ arch-chroot /mnt bash /root/arch-install/x86_64/09_nm.sh $NMINSTALL
 arch-chroot /mnt bash /root/arch-install/x86_64/10_user.sh $_USERNAME $USERPASS $SSHINSTALL
 arch-chroot /mnt bash /root/arch-install/x86_64/11_swap.sh $DOSWAP $SWAPSIZE
 arch-chroot /mnt bash /root/arch-install/x86_64/12_earlyoom.sh $DOOOM
-arch-chroot /mnt bash /root/arch-install/x86_64/13_basics.sh
+arch-chroot /mnt bash /root/arch-install/x86_64/13_basics.sh $_USERNAME
 if [[ $SETUPTYPE == "1" ]]
 then
     arch-chroot /mnt bash /root/arch-install/x86_64/s886_headless.sh $_USERNAME
--- x86_64/13_basics.sh
+++ x86_64/13_basics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: ./13_basics.sh
+# Usage: ./13_basics.sh _USERNAME
 
 # Install some packages
 sudo pacman -S --noconfirm nano-syntax-highlighting man-pages man-db zsh crda nano make gcc patch automake autoconf pkgconf fakeroot binutils rng-tools mlocate nss-mdns
@@ -20,9 +20,8 @@ install -m 644 /root/arch-install/configs/system-wide/nsswitch.conf /etc
 
 # Install yay
 cd ~
-git clone https://aur.archlinux.org/yay.git
+sudo -u $1 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -Asi
+sudo -u $1 makepkg -Asi
 cd ~
-sudo rm -r yay
-yay -S toilet
+rm -r yay
