31b183475a45568e1b2f22ce63c063a498635d99
satcom886 <rostik.medved@gmail.com>
2020-11-09 18:49:25 +0100

    Fixed a typo

--- scripts/A_questions/07_setuptypes.sh
+++ scripts/A_questions/07_setuptypes.sh
@@ -5,5 +5,5 @@ echo "What setup type do you want?"
 echo "Available options are:"
 echo "1 - satcom886's headless setup"
 echo "2 - satcom886's Plasma setup"
-echo -n "You choice: "
+echo -n "Your choice: "
 read -r SETUPTYPE
