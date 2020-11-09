256663c7afade2578bb5fed04991e73c7b5ca472
k8ie <k8ie@mcld.eu>
2020-11-09 18:49:25 +0100

    Fixed a typo

--- scripts/A_questions/07_setuptypes.sh
+++ scripts/A_questions/07_setuptypes.sh
@@ -5,5 +5,5 @@ echo "What setup type do you want?"
 echo "Available options are:"
 echo "1 - k8ieone's headless setup"
 echo "2 - k8ieone's Plasma setup"
-echo -n "You choice: "
+echo -n "Your choice: "
 read -r SETUPTYPE
