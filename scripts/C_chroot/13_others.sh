f2a042c5658f15ab268c287dd3c303aac56c0e84
k8ie <k8ie@mcld.eu>
2021-01-03 18:54:35 +0100

    Enabled reflector.timer, closes #7

--- scripts/C_chroot/13_others.sh
+++ scripts/C_chroot/13_others.sh
@@ -3,14 +3,14 @@
 # Usage: ./13_basics.sh _USERNAME
 
 # Install some packages
-sudo pacman -S --noconfirm nano-syntax-highlighting man-pages man-db zsh crda nano make gcc patch automake autoconf pkgconf fakeroot binutils rng-tools mlocate nss-mdns
+sudo pacman -S --noconfirm nano-syntax-highlighting man-pages man-db zsh crda reflector nano make gcc patch automake autoconf pkgconf fakeroot binutils rng-tools mlocate nss-mdns
 
 # Enable nano syntax highlighting
 echo "include \"/usr/share/nano/*.nanorc\"" | sudo tee -a /etc/nanorc
 echo "include \"/usr/share/nano-syntax-highlighting/*.nanorc\"" | sudo tee -a /etc/nanorc
 
 # Enable some basic services
-systemctl enable rngd man-db.timer updatedb.timer systemd-resolved.service avahi-daemon.service
+systemctl enable rngd man-db.timer updatedb.timer systemd-resolved.service avahi-daemon.service reflector.timer
 
 # Enable .local mDNS resolution
 sed -i 's/hosts:.*/hosts: files mymachines myhostname mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] dns/g' /etc/nsswitch.conf
