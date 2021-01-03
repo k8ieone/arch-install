0f19be8369ec8c9f45afdea6088efdb6fc032288
k8ie <k8ie@mcld.eu>
2021-01-03 18:55:09 +0100

    Added a GNOME setuptype

--- scripts/A_questions/07_setuptypes.sh
+++ scripts/A_questions/07_setuptypes.sh
@@ -5,5 +5,6 @@ echo "What setup type do you want?"
 echo "Available options are:"
 echo "1 - k8ieone's headless setup"
 echo "2 - k8ieone's Plasma setup"
+echo "3 - k8ieone's GNOME setup"
 echo -n "Your choice: "
 read -r SETUPTYPE
