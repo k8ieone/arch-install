60fa4aec70dd1263df105b400464dd8d29a5e692
k8ieone <rostik.medved@gmail.com>
2022-09-18 00:48:27 +0200

    Fixed the sudoers editing

--- scripts/B_actions/05_sudo.sh
+++ scripts/B_actions/05_sudo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## wheel members have sudo rights
-sudo sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /mnt/etc/sudoers
+sudo sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL:ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /mnt/etc/sudoers
