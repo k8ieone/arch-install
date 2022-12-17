fa4381bb0cdb033968371c6fcd31cf70f62187f3
k8ieone <rostik.medved@gmail.com>
2022-12-17 21:59:41 +0100

    Fix trying to write files even when answering no

--- scripts/C_chroot/09_nm.sh
+++ scripts/C_chroot/09_nm.sh
@@ -12,10 +12,9 @@ if [[ $1 == y* || $1 == "" || $1 == Y* ]]
 then
     pacman -S --noconfirm networkmanager
     systemctl enable NetworkManager.service
+    # Stop pings to archlinux.org
+    echo "[connectivity]" >> /etc/NetworkManager/NetworkManager.conf
+    echo "uri=" >> /etc/NetworkManager/NetworkManager.conf
 else
     echo "${red}Not${reset} installing NetworkManager."
 fi
-
-# Stop pings to archlinux.org
-echo "[connectivity]" >> /etc/NetworkManager/NetworkManager.conf
-echo "uri=" >> /etc/NetworkManager/NetworkManager.conf
