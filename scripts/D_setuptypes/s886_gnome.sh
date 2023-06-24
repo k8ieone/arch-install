ccba70fd3692864f2c4d852d78ead84a6bc5344e
a13xie@Sodium <rostik.medved@gmail.com>
2023-06-24 20:59:03 +0200

    Switch to Pragtical

--- scripts/D_setuptypes/s886_gnome.sh
+++ scripts/D_setuptypes/s886_gnome.sh
@@ -16,13 +16,24 @@ systemctl enable gdm.service
 # Install some additional programs
 sudo -u $1 pikaur -S --noconfirm --needed mpv gnome-shell-extension-appindicator-git \
     gnome-shell-extension-caffeine-git gnome-shell-extension-clipboard-indicator-git gnome-shell-extension-freon-git \
-    lite-xl lpm-git fontconfig
+    pragtical-git ppm-git fontconfig
 
 # Uninstall unneeded GNOME packages
 pacman -Rns --noconfirm gnome-software totem gnome-music gnome-photos orca yelp gnome-user-docs
 
-# Install Lite-XL plugins
-lpm install minimap motiontrail autoinsert bracketmatch editorconfig \
-            lsp widget snippets lintplus gitdiff_highlight gitstatus \
-            indentguide search_ui fontconfig nerdicons \
-            language_yaml language_ignore language_sh
+# Install Pragtical plugins
+
+sudo -u $1 git clone https://github.com/pragtical/lsp /home/$1/.config/pragtical/plugins/lsp
+sudo -u $1 wget https://raw.githubusercontent.com/pragtical/plugins/master/plugins/language_pkgbuild.lua -O /home/$1/.config/pragtical/plugins/language_pkgbuild.lua
+sudo -u $1 ppm install minimap autoinsert bracketmatch \
+            snippets lintplus lsp_snippets lspkind smartopenselected \
+            gitdiff_highlight gitstatus selectionhighlight \
+            colorpreview spellcheck \
+            copyfilelocation ghmarkdown restoretabs scalestatus typingspeed wordcount \
+            indentguide nerdicons regexreplacepreview todotreeview \
+            language_yaml \
+            language_csharp language_diff \
+            language_env language_ignore \
+            language_ini language_java \
+            language_nginx  language_powershell \
+            language_psql language_toml
