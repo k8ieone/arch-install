77bc63c21bc0732eb32db42c601ee1f976447e60
a13xie@Sodium <rostik.medved@gmail.com>
2023-06-25 09:10:39 +0200

    Enable multilib and add repo mirror

--- scripts/A_questions/00_before.sh
+++ scripts/A_questions/00_before.sh
@@ -19,3 +19,30 @@ fi
 # Set the console keymap
 loadkeys cz-qwertz
 echo "Keyboard layout: cz-qwertz"
+
+# Enable Multilib
+if grep 'multilib' /etc/pacman.conf
+then
+	echo "Image is x86"
+	echo "Enabling Multilib"
+	sed -i 's/#\[multilib\]/[multilib]/g' /etc/pacman.conf
+	sed -i '/\[multilib\]/a Server = https://repo.mcld.eu/repo/archlinux/$repo/os/$arch\nInclude = /etc/pacman.d/mirrorlist' /etc/pacman.conf
+else
+	echo "Not x86"
+fi
+
+# Add repo mirror
+if grep 'CARCH="x86_64"' /etc/makepkg.conf
+then
+	echo "Image is x86"
+	echo "Adding x86 mirror"
+	sed -i '/\[core\]/a Server = https://repo.mcld.eu/repo/archlinux/$repo/os/$arch' /etc/pacman.conf
+	sed -i '/\[extra\]/a Server = https://repo.mcld.eu/repo/archlinux/$repo/os/$arch' /etc/pacman.conf
+elif grep 'CARCH="aarch64"' /etc/makepkg.conf
+then
+	echo "Image is ARM64"
+	echo "Adding ARM64 mirror"
+	sed -i '/\[core\]/a Server = https://repo.mcld.eu/repo/arm/$arch/$repo' /etc/pacman.conf
+	sed -i '/\[extra\]/a Server = https://repo.mcld.eu/repo/arm/$arch/$repo' /etc/pacman.conf
+	sed -i '/\[community\]/a Server = https://repo.mcld.eu/repo/arm/$arch/$repo' /etc/pacman.conf
+fi
