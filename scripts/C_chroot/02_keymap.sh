3130f13e79520d2342b9e26a10168b8c52a2bde8
a13xie@Sodium <rostik.medved@gmail.com>
2023-06-25 10:04:47 +0200

    Minor tweaks

--- scripts/C_chroot/02_keymap.sh
+++ scripts/C_chroot/02_keymap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Hard-coded for now, shouldn't be too hard to make a selectable keymap
+# TODO: Hard-coded for now, shouldn't be too hard to make a selectable keymap
 echo "KEYMAP=cz-qwertz" > /etc/vconsole.conf
