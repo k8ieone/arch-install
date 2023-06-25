3446b8f17bde001a9bcaad59c3c52cf07f33c96c
a13xie@Sodium <rostik.medved@gmail.com>
2023-06-25 10:19:09 +0200

    noconfirm for zram-generator

--- scripts/C_chroot/11_swap.sh
+++ scripts/C_chroot/11_swap.sh
@@ -4,7 +4,7 @@
 
 if [[ $1 == y* || $1 == "" || $1 == Y* ]]
 then
-    pacman -S zram-generator
+    pacman -S --noconfirm zram-generator
     echo "[zram0]" >> /etc/systemd/zram-generator.conf
     echo "zram-size=ram / 2" >> /etc/systemd/zram-generator.conf
 fi
