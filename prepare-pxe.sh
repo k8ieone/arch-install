73c37f3742cbd7f66d1ac02925b0f0721c0f637f
k8ie <k8ie@mcld.eu>
2023-09-10 18:17:34 +0200

    Add script to prepare mounts

---
+++ prepare-pxe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -xe
+
+nbd-client -N ${$1,,} 10.0.0.3
+mkfs.ext4 /dev/nbd0
+mount /dev/nbd0 /mnt
+mkdir /mnt/boot
+mkdir -p /mnt/var/cache/pacman/pkg
+mount 10.0.0.3:/srv/atftp/arch/$1 /mnt/boot
+mount 10.0.0.3:/srv/nfs/pacman_cache/x86 /mnt/var/cache/pacman/pkg
