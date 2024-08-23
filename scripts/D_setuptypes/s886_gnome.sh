757cc3fef494b34964c6f7185a810b71bae2657f
Kateřina Medvědová <k8ie@mcld.eu>
2024-08-23 21:23:12 +0200

    Remove plugins setup

--- scripts/D_setuptypes/s886_gnome.sh
+++ scripts/D_setuptypes/s886_gnome.sh
@@ -20,19 +20,3 @@ sudo -u $1 pikaur -S --noconfirm --needed mpv gnome-shell-extension-appindicator
 
 # Uninstall unneeded GNOME packages
 pacman -Rns --noconfirm gnome-software totem gnome-music orca yelp gnome-user-docs
-
-# Install Pragtical plugins
-
-sudo -u $1 git clone https://github.com/pragtical/lsp /home/$1/.config/pragtical/plugins/lsp
-sudo -u $1 git clone https://github.com/TorchedSammy/lite-xl-lspkind.git /home/$1/.config/pragtical/plugins/lspkind
-sudo -u $1 git clone https://github.com/vqns/lite-xl-snippets.git /home/$1/.config/pragtical/plugins/lsp_snippets
-sudo -u $1 wget https://raw.githubusercontent.com/pragtical/plugins/master/plugins/language_pkgbuild.lua -O /home/$1/.config/pragtical/plugins/language_pkgbuild.lua
-sudo -u $1 wget https://raw.githubusercontent.com/pragtical/plugins/master/plugins/smartopenselected.lua -O /home/$1/.config/pragtical/plugins/smartopenselected.lua
-sudo -u $1 wget https://raw.githubusercontent.com/pragtical/plugins/master/plugins/gitstatus.lua -O /home/$1/.config/pragtical/plugins/gitstatus.lua
-sudo -u $1 wget https://raw.githubusercontent.com/pragtical/plugins/master/plugins/todotreeview.lua -O /home/$1/.config/pragtical/plugins/todotreeview.lua
-sudo -u $1 ppm install minimap autoinsert bracketmatch \
-            snippets lintplus \
-            gitdiff_highlight selectionhighlight \
-            colorpreview spellcheck memoryusage \
-            copyfilelocation ghmarkdown restoretabs scalestatus typingspeed wordcount \
-            indentguide nerdicons regexreplacepreview
