96b62b5262b377aea69d86499b6633b9ad26ed7d
k8ie <k8ie@mcld.eu>
2024-12-21 13:41:47 +0100

    Remove a whole bunch of stuff

--- scripts/D_setuptypes/s886_headless.sh
+++ scripts/D_setuptypes/s886_headless.sh
@@ -4,39 +4,10 @@
 
 set -e
 
-# Splash
-echo "   _____      _______ _____ ____  __  __  ___   ___    __  "
-echo "  / ____|  /\|__   __/ ____/ __ \|  \/  |/ _ \ / _ \  / /  "
-echo " | (___   /  \  | | | |   | |  | | \  / | (_) | (_) |/ /_  "
-echo "  \___ \ / /\ \ | | | |   | |  | | |\/| |> _ < > _ <| '_ \ "
-echo "  ____) / ____ \| | | |___| |__| | |  | | (_) | (_) | (_) |"
-echo " |_____/_/    \_\_|  \_____\____/|_|  |_|\___/ \___/ \___/ "
-echo "                                                           "
-echo "                                                           "
-echo
-
-# Install Ansible
-pacman -S --noconfirm --needed ansible
-
-# Install oh-my-zsh and plugins
-sudo -u $1 pikaur -S --noconfirm --needed oh-my-zsh-git zsh-theme-powerlevel10k zsh-syntax-highlighting zsh-autosuggestions
-
 # Install command line utilities
-pacman -S --noconfirm --needed thefuck docker libvirt eza
+pacman -S --noconfirm --needed docker libvirt
 
 systemctl enable docker.socket libvirtd.socket
 
 gpasswd -a $1 libvirt
 gpasswd -a $1 docker
-
-# Go to the directory with playbooks
-mv /root/arch-install/ansible /
-cd /ansible
-
-# Run the dotfiles playbook
-sudo -u $1 ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 dotfiles.playbook.yaml
-
-cd /
-rm -r /ansible
-# Run the system configs playbook
-# TODO: Create the system configs playbook
